c37fa62c45769ec1a724c7ff8f8ef1d9757157ec
Michael Splaver <michaelsplaver@gmail.com>
2024-02-22 15:14:41 -0600

    Update bootstrap and switch to dark mode

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,12 +4,20 @@ namespace MichaelSplaver_MidtermProjectCSI350.Controllers
 {
     public class HomeController : Controller
     {
+        [Route("")]
         public IActionResult Index()
         {
             return View();
         }
-        
+
+        [Route("ContactUs")]
         public IActionResult ContactUs()
+        {
+            return View();
+        }
+
+        [Route("UserGuide")]
+        public IActionResult UserGuide()
         {
             return View();
         }
